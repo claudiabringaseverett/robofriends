d8c3270105615eec565677005c2ecba6f593cfdd
Claudia Bringas-Everett <claudiaeverett9@gmail.com>
2020-04-25 16:29:25 -0400

    made changes on app.js

--- src/robots.js
+++ src/robots.js
@@ -58,5 +58,11 @@ export const robots = [
     name: 'Clementina DuBuque',
     username: 'Moriah.Stanton',
     email: 'Rey.Padberg@karina.biz'
+  },
+  {
+    id: 11,
+    name: 'Alessandra Everett',
+    username: 'Alessandra',
+    email: 'ale.ev@gmail.com'
   }
 ];
