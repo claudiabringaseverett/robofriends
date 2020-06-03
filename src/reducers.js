668bf98463d19cd877ce27d44cfea1ce86d8a3d2
Claudia Bringas-Everett <claudiaeverett9@gmail.com>
2020-06-03 15:10:17 -0400

    added my action, reducers, constants- setting up state management

---
+++ src/reducers.js
@@ -0,0 +1,14 @@
+import { CHANGE_SEARCH_FIELD } from './constants.js';
+
+const initialState = {
+    searchField: ''
+}
+
+export const searchRobots = (state=initialStateSearch, action={}) => {
+    switch (action.type) {
+      case CHANGE_SEARCHFIELD:
+        return Object.assign({}, state, {searchField: action.payload})
+      default:
+        return state
+    }
+} 
